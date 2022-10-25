713eb87d66ee8ab9eb255602c68ae11e2034c0f1
yuri gabriel <yurig.limadossantos@gmail.com>
2022-10-25 17:05:59 -0300

    adicionando pasta React

---
+++ lpreact/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Squad-13
+      </header>
+    </div>
+  );
+}
+
+export default App;
